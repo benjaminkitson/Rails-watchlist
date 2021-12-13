98d3c6b49c146903e73a51b441a072c2d8a30a2c
Benjamin Kitson <benjaminkitson13@gmail.com>
2021-12-13 15:39:27 +0000

    added pg_search etc, reformatted movies into grid and added bottom gradient

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,4 +3,11 @@ class Movie < ApplicationRecord
   has_many :lists
   validates :title, uniqueness: true, presence: true
   validates :overview, presence: true
+
+  include PgSearch::Model
+  pg_search_scope :search_by_title,
+    against: [:title],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
